8d669f70bd46e88a60baf927ee0f5f212935039e
Ali Abdallah <ali.abdallah@sou.inteli.edu.br>
2026-09-18 17:37:59 -0300

    Criando Primeira exportação de tela do dashboard para o App

---
+++ the-simonu/src/pages/Dashboard.tsx
@@ -0,0 +1,13 @@
+function Dashboard() {
+  return (
+    <main>
+      <h1>THE SIMONU</h1>
+
+      <p>
+        Teatro de Operações - Invasão Americana no Irã(2027)
+      </p>
+    </main>
+  )
+}
+
+export default Dashboard
